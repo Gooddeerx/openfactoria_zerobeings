f24782395b45e7d5e994709a541a2bb9e867a727
CalculatingZero <20590197+CalculatingZero@users.noreply.github.com>
2023-06-07 19:28:33 -0700

    domain and directory update

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const d = dirname(filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'docs',
+    outDir: '/docs',
     rollupOptions: {
       input: {
         index: resolve(d, 'index.html'),
